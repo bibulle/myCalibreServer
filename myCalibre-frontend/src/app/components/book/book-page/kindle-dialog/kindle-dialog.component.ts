4afa1ccc264e733eb36a90cee4a466f9c55b562a
bibulle <famille.martin@gmail.com>
2017-04-18 17:52:28 +0200

    myCalibreServer-20 : Add amazon emails to an account (dialog done)

--- myCalibre-frontend/src/app/components/book/book-page/kindle-dialog/kindle-dialog.component.ts
+++ myCalibre-frontend/src/app/components/book/book-page/kindle-dialog/kindle-dialog.component.ts
@@ -1,23 +1,41 @@
 import { Component, OnInit, Optional } from '@angular/core';
 import { MdDialogRef } from "@angular/material";
+import {UserService} from "../../../authent/user.service";
+import {User} from "../../../authent/user";
 
 @Component({
   selector: 'app-kindle-dialog',
   templateUrl: './kindle-dialog.component.html',
-  styleUrls: ['./kindle-dialog.component.scss']
+  styleUrls: ['./kindle-dialog.component.scss'],
 })
 export class KindleDialogComponent implements OnInit {
 
 
+  user: User = new User({});
+  filteredMails: String[];
+
   mail: string = "";
 
 
-  constructor(@Optional() public dialogRef: MdDialogRef<KindleDialogComponent>) { }
+  constructor(@Optional() public dialogRef: MdDialogRef<KindleDialogComponent>,
+              private _userService: UserService) { }
 
   ngOnInit() {
+    this.user = this._userService.getUser();
+    this.mailFilter();
   }
 
   send() {
+    // Add book to list if not exists
+    const found = this.user.local.amazonEmails.filter(el => {
+      return el.trim() === this.mail;
+    });
+    if (found.length == 0) {
+      this.user.local.amazonEmails.push(this.mail);
+      //noinspection JSIgnoredPromiseFromCall
+      this._userService.save(this.user);
+    }
+
     this.dialogRef.close(this.mail);
   }
 
@@ -25,4 +43,15 @@ export class KindleDialogComponent implements OnInit {
     this.dialogRef.close();
   }
 
+
+  mailFilter() {
+    if (!this.user || !this.user.local || !this.user.local.amazonEmails) {
+      return [];
+    }
+    if (this.mail) {
+      this.filteredMails = this.user.local.amazonEmails.filter(s => new RegExp(`^${this.mail}`, 'gi').test(s))
+    } else {
+      this.filteredMails = this.user.local.amazonEmails
+    }
+  }
 }
