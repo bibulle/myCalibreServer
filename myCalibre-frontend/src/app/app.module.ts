eb46eae246004ca3d6538bf5eb70abe993f4ab36
Martin, Eric (DI NB) - AF <eric.martin@airfrance.fr>
2018-04-13 16:02:09 +0200

    change whitelisteDomains

--- myCalibre-frontend/src/app/app.module.ts
+++ myCalibre-frontend/src/app/app.module.ts
@@ -48,7 +48,7 @@ import {UserService} from './components/authent/user.service';
     JwtModule.forRoot({
       config: {
         tokenGetter: UserService.tokenGetter,
-        whitelistedDomains: ['localhost:4000', 'bib.bibulle.fr', /^null$/ ]
+        whitelistedDomains: ['localhost:4000', 'bib.bibulle.fr', new RegExp('^null$') ]
       }
     }),
     FlexLayoutModule,
